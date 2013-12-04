91ea925444f6fce8a762856af8407a16f94e9eda
Jian Lin <jian@box.com>
2013-12-04 15:12:50 -0800

    Update HelloWorldApplication.java

--- HelloWorld2/src/com/example/helloworld/HelloWorldApplication.java
+++ HelloWorld2/src/com/example/helloworld/HelloWorldApplication.java
@@ -6,9 +6,9 @@ import com.box.boxandroidlibv2.BoxAndroidClient;
 
 public class HelloWorldApplication extends Application {
 
-    public static final String CLIENT_ID = "qfvqglm6w1to95y6tib4cqctj0uw7y1m";
-    public static final String CLIENT_SECRET = "7mBSlDyfqrG5SkL7xI2ZKuQ7Hv9bQf8q";
-    public static final String REDIRECT_URL = "https://app.box.com/services/ddpce_test2";
+    public static final String CLIENT_ID = "";
+    public static final String CLIENT_SECRET = "";
+    public static final String REDIRECT_URL = "";
 
     private BoxAndroidClient mClient;
 
