bd1c4f040d8e9b234095d1bd6f604a030633a39e
jian lin <jian@box.com>
2014-04-14 11:30:00 -0700

    add sdk version number in all api requests

--- BoxAndroidLibraryV2/src/com/box/boxandroidlibv2/BoxAndroidConfigBuilder.java
+++ BoxAndroidLibraryV2/src/com/box/boxandroidlibv2/BoxAndroidConfigBuilder.java
@@ -4,11 +4,15 @@ import com.box.boxjavalibv2.BoxConfigBuilder;
 
 public class BoxAndroidConfigBuilder extends BoxConfigBuilder {
 
+    /** sdk version number */
+    private final static String VERSION_NUMBER = "v3.0.3";
+
     /** Default User-Agent String. */
     private static final String USER_AGENT = "BoxAndroidLibraryV2";
 
     public BoxAndroidConfigBuilder() {
         super();
         this.setUserAgent(USER_AGENT);
+        this.setVersion(VERSION_NUMBER);
     }
 }
